d1c6e523572ca4f70191acb51186d86bd504abee
Lutfi Khuril <lutfi.khuril007@gmail.com>
2019-04-23 14:21:44 +0700

    harga pasar

--- frontend/views/pasar/index.php
+++ frontend/views/pasar/index.php
@@ -1,5 +1,7 @@
 <?php
+use yii\helpers\Html; 
 $this->title = "Harga Pasar";
+
 ?>
                    
 <div class="single-page">
@@ -14,57 +16,25 @@ $this->title = "Harga Pasar";
 		<div class="filter-pasar">
 			<!-- Single button -->
 			<div class="btn-group">
-			  <select class="form-control">
-			    <option>-- Filter --</option>
-			    <optgroup label="Beras">
-				    <option>Impor</option>
-				    <option>Ekspor</option>
-				</optgroup>
-			    <optgroup label="Jagung">
-				    <option>Impor</option>
-				    <option>Ekspor</option>
-				</optgroup>
-			  </select>
+				<?php echo Html::dropDownList('kabupaten', '', ['M'=>'Male', 'F'=>'Female'], ['prompt' => 'Pilih Kota/Kabupaten']); ?>
 			</div>
 		</div>
 	</div>
 	
 	<div class="row pasar">
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-			<div class="card">
-			  <div class="card-body">
-			    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-			      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-1" aria-expanded="false" aria-controls="coll-pasar-1">
-			        Show <span class="caret"></span>
-			      </button>
-			    </p>
-			    <div class="collapse" id="coll-pasar-1">
-			      <div class="card-body">
-			      		<img src="" style="width: 100%; height: auto;">
-			        	<p>Pasar Johar Rp. 12,000</p>
-						<p>Pasar Peterongan Rp. 12,000</p>
-						<p>Pasar Karangayu Rp. 12,000</p>
-						<p>Pasar Bulu Rp. 11,700</p>
-						<p>Pasar Gayamsari Rp. 11,700</p>
-			      </div>
-			    </div>
-			    <div class="card-footer">
-				     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-				     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-			    </div>
-			  </div>
-			</div>
-		</div>
+		<?php 
+		$col = 1;
+		foreach ($item as $key => $value) { ?>
 
 		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
 			<div class="card">
 			  <div class="card-body">
 			    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-			      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-2" aria-expanded="false" aria-controls="coll-pasar-2">
+			      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-<?= $col ?>" aria-expanded="false" aria-controls="coll-pasar-<?= $col ?>">
 			        Show <span class="caret"></span>
 			      </button>
 			    </p>
-			    <div class="collapse" id="coll-pasar-2">
+			    <div class="collapse" id="coll-pasar-<?= $col ?>">
 			      <div class="card-body">
 			      		<img src="" style="width: 100%; height: auto;">
 			        	<p>Pasar Johar Rp. 12,000</p>
@@ -81,32 +51,9 @@ $this->title = "Harga Pasar";
 			  </div>
 			</div>
 		</div>
-
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-			<div class="card">
-			  <div class="card-body">
-			    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-			      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-3" aria-expanded="false" aria-controls="coll-pasar-3">
-			        Show <span class="caret"></span>
-			      </button>
-			    </p>
-			    <div class="collapse" id="coll-pasar-3">
-			      <div class="card-body">
-			      		<img src="" style="width: 100%; height: auto;">
-			        	<p>Pasar Johar Rp. 12,000</p>
-						<p>Pasar Peterongan Rp. 12,000</p>
-						<p>Pasar Karangayu Rp. 12,000</p>
-						<p>Pasar Bulu Rp. 11,700</p>
-						<p>Pasar Gayamsari Rp. 11,700</p>
-			      </div>
-			    </div>
-			    <div class="card-footer">
-				     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-				     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-			    </div>
-			  </div>
-			</div>
-		</div>
+		<?php 
+			$col++;
+		} ?>
 	</div>
 
 </div>
