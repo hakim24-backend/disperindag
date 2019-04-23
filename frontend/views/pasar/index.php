b82758a7ba1de44ef607cfcd01cb63766614f614
farisfri <farisfriansyah95@gmail.com>
2019-04-23 13:43:21 +0700

    filter

--- frontend/views/pasar/index.php
+++ frontend/views/pasar/index.php
@@ -11,7 +11,21 @@ $this->title = "Harga Pasar";
 	</div>
 
 	<div class="row">
-		<div class=""></div>
+		<div class="filter-pasar">
+			<!-- Single button -->
+			<div class="btn-group">
+			  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			    Filter <span class="caret"></span>
+			  </button>
+			  <ul class="dropdown-menu">
+			    <li><a href="#">Action</a></li>
+			    <li><a href="#">Another action</a></li>
+			    <li><a href="#">Something else here</a></li>
+			    <li role="separator" class="divider"></li>
+			    <li><a href="#">Separated link</a></li>
+			  </ul>
+			</div>
+		</div>
 	</div>
 	
 	<div class="row pasar">
