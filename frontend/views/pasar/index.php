b43c73d91a6e0880b789f45046395715a680650f
farisfri <farisfriansyah95@gmail.com>
2019-04-23 18:24:18 +0700

    fixed

--- frontend/views/pasar/index.php
+++ frontend/views/pasar/index.php
@@ -26,31 +26,35 @@ $this->title = "Harga Pasar";
 		$col = 1;
 		foreach ($item as $key => $value) { ?>
 
-		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 pasar-content">
+		<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 pasar-content">
 			<div class="card">
 			  <div class="card-body">
-			    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-			      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-<?= $col ?>" aria-expanded="false" aria-controls="coll-pasar-<?= $col ?>">
-			        Show <span class="caret"></span>
-			      </button>
-			    </p>
-			    <div class="collapse" id="coll-pasar-<?= $col ?>">
-			      <div class="card-body">
-			      		<img src="" style="width: 100%; height: auto;">
-			        	<p>Pasar Johar Rp. 12,000</p>
-						<p>Pasar Peterongan Rp. 12,000</p>
-						<p>Pasar Karangayu Rp. 12,000</p>
-						<p>Pasar Bulu Rp. 11,700</p>
-						<p>Pasar Gayamsari Rp. 11,700</p>
-			      </div>
-			    </div>
-			    <div class="card-footer">
-				     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-				     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-			    </div>
+	  		  	<div style="display: inline-block;">
+	  			    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
+	  			      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-<?= $col ?>" aria-expanded="false" aria-controls="coll-pasar-<?= $col ?>">
+	  			        Show <span class="caret"></span>
+	  			      </button>
+	  			    </p>
+	  			    <div class="collapse" id="coll-pasar-<?= $col ?>">
+	  			      <div class="card-body">
+	  			      		<img src="" style="width: 100%; height: auto;">
+	  			        	<p>Pasar Johar Rp. 12,000</p>
+	  						<p>Pasar Peterongan Rp. 12,000</p>
+	  						<p>Pasar Karangayu Rp. 12,000</p>
+	  						<p>Pasar Bulu Rp. 11,700</p>
+	  						<p>Pasar Gayamsari Rp. 11,700</p>
+	  			      </div>
+	  			    </div>
+	  			</div>
+	  			<div style="float: right; display: inline-block;">
+	  				 <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
+	  			     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
+	  			</div>
 			  </div>
 			</div>
 		</div>
+
+
 		<?php 
 			$col++;
 		} ?>
