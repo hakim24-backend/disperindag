56f2863d43533318bb6850fba66f2b0087a50941
farisfri <farisfriansyah95@gmail.com>
2019-04-23 16:40:14 +0700

    back

--- frontend/views/pasar/index.php
+++ frontend/views/pasar/index.php
@@ -22,231 +22,38 @@ $this->title = "Harga Pasar";
 	</div>
 	
 	<div class="row pasar">
-		<div class="col-md-4 pasar-1">
+		<?php 
+		$col = 1;
+		foreach ($item as $key => $value) { ?>
 
-				<div class="card pasar-content">
-				  <div class="card-body">
-				    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-				      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-1-1" aria-expanded="false" aria-controls="coll-pasar-1-1">
-				        Show <span class="caret"></span>
-				      </button>
-				    </p>
-				    <div class="collapse" id="coll-pasar-1-1">
-				      <div class="card-body">
-				      		<img src="" style="width: 100%; height: auto;">
-				        	<p>Pasar Johar Rp. 12,000</p>
-							<p>Pasar Peterongan Rp. 12,000</p>
-							<p>Pasar Karangayu Rp. 12,000</p>
-							<p>Pasar Bulu Rp. 11,700</p>
-							<p>Pasar Gayamsari Rp. 11,700</p>
-				      </div>
-				    </div>
-				    <div class="card-footer">
-					     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-					     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-				    </div>
-				  </div>
-				</div>
-
-				<div class="card pasar-content">
-				  <div class="card-body">
-				    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-				      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-1-2" aria-expanded="false" aria-controls="coll-pasar-1-2">
-				        Show <span class="caret"></span>
-				      </button>
-				    </p>
-				    <div class="collapse" id="coll-pasar-1-2">
-				      <div class="card-body">
-				      		<img src="" style="width: 100%; height: auto;">
-				        	<p>Pasar Johar Rp. 12,000</p>
-							<p>Pasar Peterongan Rp. 12,000</p>
-							<p>Pasar Karangayu Rp. 12,000</p>
-							<p>Pasar Bulu Rp. 11,700</p>
-							<p>Pasar Gayamsari Rp. 11,700</p>
-				      </div>
-				    </div>
-				    <div class="card-footer">
-					     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-					     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-				    </div>
-				  </div>
-				</div>
-
-				<div class="card pasar-content">
-				  <div class="card-body">
-				    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-				      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-1-3" aria-expanded="false" aria-controls="coll-pasar-1-3">
-				        Show <span class="caret"></span>
-				      </button>
-				    </p>
-				    <div class="collapse" id="coll-pasar-1-3">
-				      <div class="card-body">
-				      		<img src="" style="width: 100%; height: auto;">
-				        	<p>Pasar Johar Rp. 12,000</p>
-							<p>Pasar Peterongan Rp. 12,000</p>
-							<p>Pasar Karangayu Rp. 12,000</p>
-							<p>Pasar Bulu Rp. 11,700</p>
-							<p>Pasar Gayamsari Rp. 11,700</p>
-				      </div>
-				    </div>
-				    <div class="card-footer">
-					     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-					     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-				    </div>
-				  </div>
-				</div>
+		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 pasar-content">
+			<div class="card">
+			  <div class="card-body">
+			    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
+			      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-<?= $col ?>" aria-expanded="false" aria-controls="coll-pasar-<?= $col ?>">
+			        Show <span class="caret"></span>
+			      </button>
+			    </p>
+			    <div class="collapse" id="coll-pasar-<?= $col ?>">
+			      <div class="card-body">
+			      		<img src="" style="width: 100%; height: auto;">
+			        	<p>Pasar Johar Rp. 12,000</p>
+						<p>Pasar Peterongan Rp. 12,000</p>
+						<p>Pasar Karangayu Rp. 12,000</p>
+						<p>Pasar Bulu Rp. 11,700</p>
+						<p>Pasar Gayamsari Rp. 11,700</p>
+			      </div>
+			    </div>
+			    <div class="card-footer">
+				     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
+				     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
+			    </div>
+			  </div>
+			</div>
 		</div>
-
-		<div class="col-md-4 pasar-2">
-
-				<div class="card pasar-content">
-				  <div class="card-body">
-				    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-				      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-2-1" aria-expanded="false" aria-controls="coll-pasar-2-1">
-				        Show <span class="caret"></span>
-				      </button>
-				    </p>
-				    <div class="collapse" id="coll-pasar-2-1">
-				      <div class="card-body">
-				      		<img src="" style="width: 100%; height: auto;">
-				        	<p>Pasar Johar Rp. 12,000</p>
-							<p>Pasar Peterongan Rp. 12,000</p>
-							<p>Pasar Karangayu Rp. 12,000</p>
-							<p>Pasar Bulu Rp. 11,700</p>
-							<p>Pasar Gayamsari Rp. 11,700</p>
-				      </div>
-				    </div>
-				    <div class="card-footer">
-					     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-					     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-				    </div>
-				  </div>
-				</div>
-
-				<div class="card pasar-content">
-				  <div class="card-body">
-				    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-				      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-2-2" aria-expanded="false" aria-controls="coll-pasar-2-2">
-				        Show <span class="caret"></span>
-				      </button>
-				    </p>
-				    <div class="collapse" id="coll-pasar-2-2">
-				      <div class="card-body">
-				      		<img src="" style="width: 100%; height: auto;">
-				        	<p>Pasar Johar Rp. 12,000</p>
-							<p>Pasar Peterongan Rp. 12,000</p>
-							<p>Pasar Karangayu Rp. 12,000</p>
-							<p>Pasar Bulu Rp. 11,700</p>
-							<p>Pasar Gayamsari Rp. 11,700</p>
-				      </div>
-				    </div>
-				    <div class="card-footer">
-					     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-					     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-				    </div>
-				  </div>
-				</div>
-
-				<div class="card pasar-content">
-				  <div class="card-body">
-				    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-				      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-2-3" aria-expanded="false" aria-controls="coll-pasar-2-3">
-				        Show <span class="caret"></span>
-				      </button>
-				    </p>
-				    <div class="collapse" id="coll-pasar-2-3">
-				      <div class="card-body">
-				      		<img src="" style="width: 100%; height: auto;">
-				        	<p>Pasar Johar Rp. 12,000</p>
-							<p>Pasar Peterongan Rp. 12,000</p>
-							<p>Pasar Karangayu Rp. 12,000</p>
-							<p>Pasar Bulu Rp. 11,700</p>
-							<p>Pasar Gayamsari Rp. 11,700</p>
-				      </div>
-				    </div>
-				    <div class="card-footer">
-					     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-					     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-				    </div>
-				  </div>
-				</div>
-		</div>
-
-		<div class="col-md-4 pasar-3">
-
-				<div class="card pasar-content">
-				  <div class="card-body">
-				    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-				      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-3-1" aria-expanded="false" aria-controls="coll-pasar-3-1">
-				        Show <span class="caret"></span>
-				      </button>
-				    </p>
-				    <div class="collapse" id="coll-pasar-3-1">
-				      <div class="card-body">
-				      		<img src="" style="width: 100%; height: auto;">
-				        	<p>Pasar Johar Rp. 12,000</p>
-							<p>Pasar Peterongan Rp. 12,000</p>
-							<p>Pasar Karangayu Rp. 12,000</p>
-							<p>Pasar Bulu Rp. 11,700</p>
-							<p>Pasar Gayamsari Rp. 11,700</p>
-				      </div>
-				    </div>
-				    <div class="card-footer">
-					     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-					     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-				    </div>
-				  </div>
-				</div>
-
-
-				<div class="card pasar-content">
-				  <div class="card-body">
-				    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-				      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-3-2" aria-expanded="false" aria-controls="coll-pasar-3-2">
-				        Show <span class="caret"></span>
-				      </button>
-				    </p>
-				    <div class="collapse" id="coll-pasar-3-2">
-				      <div class="card-body">
-				      		<img src="" style="width: 100%; height: auto;">
-				        	<p>Pasar Johar Rp. 12,000</p>
-							<p>Pasar Peterongan Rp. 12,000</p>
-							<p>Pasar Karangayu Rp. 12,000</p>
-							<p>Pasar Bulu Rp. 11,700</p>
-							<p>Pasar Gayamsari Rp. 11,700</p>
-				      </div>
-				    </div>
-				    <div class="card-footer">
-					     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-					     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-				    </div>
-				  </div>
-				</div>
-
-				<div class="card pasar-content">
-				  <div class="card-body">
-				    <h5 class="card-title"><strong>Beras IR64 (kw premium)</strong></h5>
-				      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#coll-pasar-3-3" aria-expanded="false" aria-controls="coll-pasar-3-3">
-				        Show <span class="caret"></span>
-				      </button>
-				    </p>
-				    <div class="collapse" id="coll-pasar-3-3">
-				      <div class="card-body">
-				      		<img src="" style="width: 100%; height: auto;">
-				        	<p>Pasar Johar Rp. 12,000</p>
-							<p>Pasar Peterongan Rp. 12,000</p>
-							<p>Pasar Karangayu Rp. 12,000</p>
-							<p>Pasar Bulu Rp. 11,700</p>
-							<p>Pasar Gayamsari Rp. 11,700</p>
-				      </div>
-				    </div>
-				    <div class="card-footer">
-					     <p style="font-size: 14px;"><small>Harga Rata-Rata</small></p>
-					     <h4 class="card-title"><strong>Rp. 11.780</strong></h4>
-				    </div>
-				  </div>
-				</div>
-		</div>
+		<?php 
+			$col++;
+		} ?>
 	</div>
 
 </div>
